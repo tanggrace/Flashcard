dbf16dcdd42386b5b0318f7a8abe7cc58077bfd4
tanggracce <tanggrace.11@gmail.com>
2020-02-15 15:19:42 -0500

    Initial commit.

---
+++ app/src/main/java/com/example/grace/flashcard/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.grace.flashcard;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+            }
+        });
+
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+            findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+        }
+    });
+    }
+}
